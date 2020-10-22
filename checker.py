19e65500667dabe7423a37c5f8cfd364b991fd30
denn9s <cs.dennisong@gmail.com>
2020-10-22 15:41:35 -0700

    Authenticating with Spotify

--- checker.py
+++ checker.py
@@ -1,2 +1,6 @@
+import spotipy
 from credentials import cid, secret
 from spotipy.oauth2 import SpotifyClientCredentials
+
+client_credentials_manager = SpotifyClientCredentials(client_id = cid, client_secret = secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
