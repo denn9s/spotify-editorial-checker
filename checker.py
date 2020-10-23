dcaad54affcf17ec4de47d231110404dca9556dd
denn9s <cs.dennisong@gmail.com>
2020-10-22 18:12:11 -0700

    Migrated playlist iteration into main function

--- checker.py
+++ checker.py
@@ -8,12 +8,18 @@ sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 label_count = defaultdict(int) # key = label name, value = amount of songs across playlists
 
-playlists = sp.user_playlists('spotify')
-count = 1
-while playlists:
-	for i, playlist in enumerate(playlists['items']):
-		pass
-	if playlists['next']:
-		playlists = sp.next(playlists)
-	else:
-		playlists = None
+output_file = io.open('output.txt', 'w', encoding = 'utf-8', errors = 'ignore')
+
+if __name__ == '__main__':
+	main()
+
+def main():
+	playlists =	 sp.user_playlists('spotify')
+	count = 1
+	while playlists:
+		for i, playlist in enumerate(playlists['items']):
+			pass
+		if playlists['next']:
+			playlists = sp.next(playlists)
+		else:
+			playlists = None
