4e3344289dcf674bd2794b854f6b5296f1e79356
denn9s <cs.dennisong@gmail.com>
2020-11-11 19:50:08 -0800

    Added file loading

--- compare.py
+++ compare.py
@@ -1,6 +1,16 @@
 import os
+import io
+
+output_root_dir = 'output'
 
 def main():
+	output_list = os.listdir(output_root_dir)
+	output_list.sort()
+	for file in output_list:
+		load(file)
+
+def load(file_name):
+	file = io.open(output_root_dir + '/' + file_name, 'r', encoding = 'utf-8')
 	pass
 
 if __name__ == '__main__':
